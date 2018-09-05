a4b864ed7244b2b61190448564a42b857f11b00c
2010YOUY01 <213163193@seu.edu.cn>
2018-09-05 18:35:05 +0800

    create prototype for loginDAO

--- src/vcampus/dao/LoginDAO.java
+++ src/vcampus/dao/LoginDAO.java
@@ -1,8 +1,11 @@
 package vcampus.dao;
 
 import vcampus.vo.LoginFormEvent;
+import vcampus.vo.RegisterForm;
 
+//其中username为学号，学生管理员都有，用于唯一确认用户身份
 public class LoginDAO {
+	//验证loginFormEvent里的用户名与密码，登陆成功返回true，否则返回false
 	public boolean LoginCheck(LoginFormEvent loginFormEvent) {
 		boolean loginSucceedFlag = false;
 		//TODO implement DAO
@@ -10,5 +13,14 @@ public class LoginDAO {
 			loginSucceedFlag = true;
 		}
 		return loginSucceedFlag;
+	}
+	
+	//通过RegisterForm里的用户名，密码，用户姓名在数据库里新增用户，注册成功返回true，否则返回false
+	//注意username为唯一学号，如果数据库中有重复，则注册失败
+	public boolean Register(RegisterForm registerForm) {
+		boolean registerSucceedFlag = false;
+		
+		
+		return registerSucceedFlag;
 	}
 }
